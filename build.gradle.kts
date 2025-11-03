dedbf4a9658e9d60b51d5f0f58e40c81497840f5
FlummoxGamer <140507968+FlummoxGamer@users.noreply.github.com>
2025-11-03 20:02:46 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
-// Line 9 is here -> this block must be exactly correct
+// *** Subprojects block is empty now to fix the Line 9 error ***
 subprojects {
-    repositories {
-        mavenCentral()
-        google()
-        maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://api.github.com/repos/recloudstream/Cloudstream-3/packages/maven/cloudstream") }
-    }
+    // Repositories moved to settings.gradle.kts
 }
