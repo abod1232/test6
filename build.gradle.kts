d404427b95b511c21dff06fafb0933e370a2757f
FlummoxGamer <140507968+FlummoxGamer@users.noreply.github.com>
2025-11-04 02:08:15 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,17 +4,3 @@ allprojects {
         mavenCentral()
     }
 }
-
-// REMOVE this entire subprojects block - it's causing conflicts
-// subprojects {
-//     repositories {
-//         maven {
-//             name = "cloudstream"
-//             url = uri("https://api.github.com/repos/recloudstream/cloudstream-3/packages/maven")
-//             credentials {
-//                 username = System.getenv("GITHUB_ACTOR")
-//                 password = System.getenv("GITHUB_TOKEN")
-//             }
-//         }
-//     }
-// }
